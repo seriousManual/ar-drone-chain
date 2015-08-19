4293429c637af81c79e7af641b3b07cbf455972f
zaphod1984 <zaphod84@gmx.de>
2015-08-19 10:24:42 +0200

    add handler tests

--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,38 @@
 var expect = require('chai').expect;
+var sinon = require('sinon');
 
-describe('foo', function() {
-    it('should', function() {
-        expect('foo').to.equal('foo');
+var HandlerCB = require('../lib/HandlerCB');
+var HandlerTimeout = require('../lib/HandlerTimeout');
+
+describe('Handlers', function() {
+    describe('HandlerCB', function() {
+        it('should call the callback', function(done) {
+            var handler = new HandlerCB(function(callback) {
+                setTimeout(callback, 100);
+            });
+
+            handler.run(done);
+        });
+    });
+
+    describe('HandlerTimeout', function() {
+        var clock;
+        before(function() {
+            clock = sinon.useFakeTimers();
+        });
+
+        after(function() {
+            clock.restore();
+        });
+
+        it('should call the handler and the callback after the specified time', function(done) {
+            var spy = sinon.spy();
+            var handler = new HandlerTimeout(100, spy);
+
+            handler.run(done);
+            expect(spy.args.length).to.equal(1);
+
+            clock.tick(100);
+        });
     });
 });
